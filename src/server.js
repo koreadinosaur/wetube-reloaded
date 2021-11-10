d7e19e02b98be707f0c95f3cdfe10021d000bca2
HyeongJin <khj7741@gmail.com>
2021-11-11 00:47:18 +0900

    study middleware

--- src/server.js
+++ src/server.js
@@ -4,9 +4,25 @@ const PORT = 4000;
 
 const app = express();
 
+const logger = (req, res, next) => {
+  console.log(`${req.method} ${req.url}`);
+  next();
+};
+const privateMiddleware = (req, res, next) => {
+  const url = req.url;
+  if (url === "/protected") {
+    return res.send("<h1>Not Allowed</h1>");
+  }
+  next();
+};
+
 const handleHome = (req, res) => {
   return res.send("hi");
 };
+const handleProtected = (req, res) => {
+  return res.send("Welcome to the private lounge");
+};
+
 const handleAbout = (req, res) => {
   return res.send("about here");
 };
@@ -17,7 +33,11 @@ const handleLogin = (req, res) => {
   res.send("login");
 };
 
+app.use(logger);
+app.use(privateMiddleware);
 app.get("/", handleHome);
+app.get("/protected", handleProtected);
+
 app.get("/about", handleAbout);
 app.get("/contact", handleContact);
 app.get("/login", handleLogin);
