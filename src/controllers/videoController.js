8e6bebb7b9ad48d26b9818b881b40d6da0421104
HyeongJin <khj7741@gmail.com>
2021-11-18 03:11:46 +0900

    #5.7-6.4

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,10 +1,51 @@
 //globalRouter
-export const trending = (req, res) => res.render("home", { pageTitle: "Home" });
+let videos = [
+  {
+    title: "First Video",
+    rating: 5,
+    comments: 2,
+    createdAt: "2 minutes ago",
+    views: 1,
+    id: 1,
+  },
+  {
+    title: "Second Video",
+    rating: 5,
+    comments: 2,
+    createdAt: "2 minutes ago",
+    views: 30,
+    id: 2,
+  },
+  {
+    title: "Third Video",
+    rating: 5,
+    comments: 2,
+    createdAt: "2 minutes ago",
+    views: 30,
+    id: 3,
+  },
+];
+export const trending = (req, res) => {
+  return res.render("home", { pageTitle: "Home", videos });
+};
 export const search = (req, res) => res.send("search");
 
 //videoRouter
-export const see = (req, res) => res.render("watch");
-export const handleEdit = (req, res) => res.render("edit");
+export const watch = (req, res) => {
+  const { id } = req.params;
+  const video = videos[id - 1];
+  return res.render("watch", { pageTitle: `Watching ${video.title}`, video });
+};
+export const getEdit = (req, res) => {
+  const { id } = req.params;
+  const video = videos[id - 1];
+  return res.render("edit", { pageTitle: `Editing ${video.title}`, video });
+};
+export const postEdit = (req, res) => {
+  const { id } = req.params;
+  console.log(req.body);
+  return res.redirect(`/videos/${id}`);
+};
 export const upload = (req, res) => res.send("upload video");
 export const deleteVideo = (req, res) => {
   console.log(req.params);
--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -1,15 +1,16 @@
 import express from "express";
 import {
-  handleEdit,
-  see,
+  getEdit,
+  watch,
   upload,
   deleteVideo,
+  postEdit,
 } from "../controllers/videoController";
 const videoRouter = express.Router();
 
 videoRouter.get("/upload", upload);
-videoRouter.get("/:id(\\d+)", see); //videos/watch라고 할 필요가 없음. 이미 /videos 안이니까
-videoRouter.get("/:id(\\d+)/edit", handleEdit);
+videoRouter.get("/:id(\\d+)", watch); //videos/watch라고 할 필요가 없음. 이미 /videos 안이니까
+videoRouter.route("/:id(\\d+)/edit").get(getEdit).post(postEdit); //위의 코드 두줄을 이렇게 만들 수 있음
 videoRouter.get("/:id(\\d+)/delete", deleteVideo);
 
 export default videoRouter;
