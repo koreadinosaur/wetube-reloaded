fe86880754ea9848add26436c83f596d4c7716c2
HyeongJin <khj7741@gmail.com>
2021-11-25 02:55:07 +0900

    callback, promise study

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,52 +1,29 @@
+import video from "../models/video";
+
 //globalRouter
-let videos = [
-  {
-    title: "First Video",
-    rating: 5,
-    comments: 2,
-    createdAt: "2 minutes ago",
-    views: 1,
-    id: 1,
-  },
-  {
-    title: "Second Video",
-    rating: 5,
-    comments: 2,
-    createdAt: "2 minutes ago",
-    views: 30,
-    id: 2,
-  },
-  {
-    title: "Third Video",
-    rating: 5,
-    comments: 2,
-    createdAt: "2 minutes ago",
-    views: 30,
-    id: 3,
-  },
-];
-export const trending = (req, res) => {
-  return res.render("home", { pageTitle: "Home", videos });
+export const home = async (req, res) => {
+  const videos = await video.find({});
+  return res.render("home", { pageTitle: "Home", videos: [] });
 };
 export const search = (req, res) => res.send("search");
 
 //videoRouter
 export const watch = (req, res) => {
   const { id } = req.params;
-  const video = videos[id - 1];
-  return res.render("watch", { pageTitle: `Watching ${video.title}`, video });
+
+  return res.render("watch", { pageTitle: `Watching` });
 };
 export const getEdit = (req, res) => {
   const { id } = req.params;
-  const video = videos[id - 1];
-  return res.render("edit", { pageTitle: `Editing ${video.title}`, video });
+
+  return res.render("edit", { pageTitle: `Editing` });
 };
 export const postEdit = (req, res) => {
   const { id } = req.params;
   const { title } = req.body;
-  videos[id - 1].title = title;
+
   console.log(req.body);
-  return res.redirect(`/videos/${id}`);
+  return res.redirect();
 };
 export const upload = (req, res) => res.send("upload video");
 export const deleteVideo = (req, res) => {
--- src/server.js
+++ src/server.js
@@ -1,11 +1,9 @@
-import "./db";
 import express from "express";
 import logger from "morgan";
 import globalRouter from "./routers/globalRouter";
 import videoRouter from "./routers/videoRouter";
 import userRouter from "./routers/userRouter";
 
-const PORT = 4000;
 const app = express();
 const morganLogger = logger("dev");
 app.use(morganLogger);
@@ -17,7 +15,4 @@ app.use("/", globalRouter);
 app.use("/videos", videoRouter);
 app.use("/users", userRouter);
 
-const handleListening = () =>
-  console.log(`server listening on port 4000 http://localhost:${PORT}`);
-
-app.listen(PORT, handleListening);
+export default app;
