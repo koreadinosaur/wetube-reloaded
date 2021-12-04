de7a23ed62ffa31bdf55e0251143ed4d570990cf
HyeongJin <khj7741@gmail.com>
2021-12-04 23:49:16 +0900

    create 404

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,4 +1,4 @@
-import video from "../models/video";
+import video from "../models/video"; //video는 model, 후술할 Video는 objcet
 
 //globalRouter
 export const home = async (req, res) => {
@@ -8,22 +8,44 @@ export const home = async (req, res) => {
 export const search = (req, res) => res.send("search");
 
 //videoRouter
-export const watch = (req, res) => {
-  const { id } = req.params;
-
-  return res.render("watch", { pageTitle: `Watching` });
+export const watch = async (req, res) => {
+  const { id } = req.params; //router가 주는 express기능인 것만 알면 됨.
+  const Video = await video.findById(id); //findById에서 필요한 id는 req.params에서 찾아온다.
+  //video.findById(id).exec()입력하면 mongoose 내부적으로 promise가 return된다. 하지만 async랑 await을 쓰고 있기 떄문에
+  // exec()를 입력할 필요는 없다. 입력해도 똑같이 작동함.
+  if (Video === null) {
+    //Video === null은 !Video와 같다.
+    return res.render("404", { pageTitle: "Video not found" });
+  } else {
+    return res.render("watch", { pageTitle: Video.title, video: Video }); //video: video가 같은 이름이면 video만 입력해도 됨.
+  }
 };
-export const getEdit = (req, res) => {
+export const getEdit = async (req, res) => {
   const { id } = req.params;
-
-  return res.render("edit", { pageTitle: `Editing` });
+  const Video = await video.findById(id);
+  if (Video === null) {
+    //Video === null은 !Video와 같다.
+    return res.render("404", { pageTitle: "Video not found" });
+  }
+  return res.render("edit", { pageTitle: `Edit ${Video.title}`, video: Video });
 };
-export const postEdit = (req, res) => {
-  const { id } = req.params;
-  const { title } = req.body;
 
-  console.log(req.body);
-  return res.redirect();
+export const postEdit = async (req, res) => {
+  const { id } = req.params;
+  const { title, description, hashtags } = req.body;
+  const Video = await video.exists({ _id: id });
+  if (Video === null) {
+    //Video === null은 !Video와 같다.
+    return res.render("404", { pageTitle: "Video not found" });
+  }
+  await video.findByIdAndUpdate(id, {
+    title,
+    description,
+    hashtags: hashtags
+      .split(",")
+      .map((word) => (word.startsWith("#") ? word : `#${word}`)),
+  });
+  return res.redirect(`/videos/${id}`);
 };
 export const getUpload = (req, res) => {
   return res.render("upload", { pageTitle: `upload Video` });
--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -9,9 +9,9 @@ import {
 } from "../controllers/videoController";
 const videoRouter = express.Router();
 
-videoRouter.route("/upload").get(getUpload).post(postUpload);
-videoRouter.get("/:id(\\d+)", watch); //videos/watch라고 할 필요가 없음. 이미 /videos 안이니까
-videoRouter.route("/:id(\\d+)/edit").get(getEdit).post(postEdit); //위의 코드 두줄을 이렇게 만들 수 있음
-videoRouter.get("/:id(\\d+)/delete", deleteVideo);
+videoRouter.route("/upload").get(getUpload).post(postUpload); //videoRouter.get("/:id(\\d+)", watch); 이녀석보다 위에 있어야함. 아니면 id로 인식된다.
+videoRouter.get("/:id([0-9a-f]{24})", watch); //videos/watch라고 할 필요가 없음. 이미 /videos 안이니까
+videoRouter.route("/:id([0-9a-f]{24})/edit").get(getEdit).post(postEdit); //위의 코드 두줄을 이렇게 만들 수 있음
+videoRouter.get("/:id([0-9a-f]{24})/delete", deleteVideo);
 
 export default videoRouter;
